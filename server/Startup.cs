024bef416698c17bf3c808153d5c3f0ade62d714
biggieliten <filipisaksson00@gmail.com>
2026-02-17 13:45:11 +0100

    asdasd

--- server/Startup.cs
+++ server/Startup.cs
@@ -1,13 +1,13 @@
-using Microsoft.Owin;
-using Owin;
+// using Microsoft.Owin;
+// using Owin;
 
-//[assembly: OwinStartup(typeof(SignalRChat.Startup))]
-namespace SignalRChat;
+// //[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+// namespace SignalRChat;
 
-public class Startup
-{
-	public void Configuration(IAppBuilder app)
-	{
-		app.MapSignalR();
-	}
-}
+// public class Startup
+// {
+// 	public void Configuration(IAppBuilder app)
+// 	{
+// 		app.MapSignalR();
+// 	}
+// }
